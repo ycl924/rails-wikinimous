f0c494874190979e55482356424e3ab76fed3fc4
Santiago Campos <ycl@outlook.com>
2019-10-22 21:18:23 +0800

    Third of Tuesday6 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+
 end
